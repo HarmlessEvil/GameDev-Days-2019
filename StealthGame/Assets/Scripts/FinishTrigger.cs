e9a3058cc3b4a644e5adac4e898b2b4520ee3602
Ellen Kartysheva <el.kartysheva@yandex.ru>
2019-10-28 20:30:17 +0300

    Add score scripts

---
+++ StealthGame/Assets/Scripts/FinishTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishTrigger : MonoBehaviour
+{
+    public GameObject player;
+    private bool triggered = false;
+    public ScoreManager EmptyScoreObj;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.gameObject == player && !triggered)
+        {
+            triggered = true;
+            EmptyScoreObj.StopTimer();
+            Debug.Log("Finish");
+            Debug.Log(EmptyScoreObj.Score());
+        }
+    }
+}
