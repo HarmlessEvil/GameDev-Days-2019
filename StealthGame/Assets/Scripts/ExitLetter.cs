c723fd0633faade63f0e79b987394c6b5dab9247
Ellen Kartysheva <el.kartysheva@yandex.ru>
2019-10-29 19:50:07 +0300

    Add scripts for letter

---
+++ StealthGame/Assets/Scripts/ExitLetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitLetter : MonoBehaviour
+{
+    public GameObject diarySprite;
+    void Update()
+    {
+        if (Input.GetButton("Cancel") && !diarySprite)
+        {
+            this.gameObject.SetActive(false);
+            Destroy(this.gameObject, 0.5f);
+        }
+    }
+}
