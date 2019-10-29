6060d3bf228f41712e95737f5e28d5ab10ae3a90
Alexander <aleksandr_chori@mail.ru>
2019-10-29 11:10:29 +0300

    Add basic soundtrack

---
+++ StealthGame/Assets/Scripts/CatchPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchPlayer : MonoBehaviour
+{
+    public EnemyController enemy;
+
+    void Update()
+    {
+        Vector3 desiredVelocity = enemy.GetDesiredVelocity();
+        if (desiredVelocity != null)
+        {
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, enemy.GetDesiredVelocity());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == enemy.GetPlayer().gameObject)
+        {
+            enemy.CatchPlayer();
+        }
+    }
+}
