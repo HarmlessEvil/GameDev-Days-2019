e61d3dc47be4009935ab5e240dd061de20af3cac
karl-crl <el.kartysheva@yandex.ru>
2019-10-30 11:07:33 +0300

    Sasha missed my diary :/

---
+++ StealthGame/Assets/Scripts/DisableDialogText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableDialogText : MonoBehaviour
+{
+    float time = 0.0f;
+    public Text dialog;
+    void Update()
+    {
+        if (dialog.text.Length > 0)
+        {
+            time += Time.deltaTime;
+            if (time > 10.0f)
+            {
+                dialog.text = "";
+                time = 0.0f;
+            }
+        }  
+    }
+}
