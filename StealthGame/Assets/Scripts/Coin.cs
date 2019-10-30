133d69b3adc4fc18183df0899ecc09184a9a91e5
Eugene <zzzheka97@gmail.com>
2019-10-30 12:00:03 +0300

    coins added

---
+++ StealthGame/Assets/Scripts/Coin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Coin : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer m_renderer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.Play("Idle");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            var inventory = other.gameObject.GetComponent<Inventory>();
+            if (inventory)
+            {
+                inventory.Add(0, 2);
+                animator.Play("KeyPicking");
+                Destroy(gameObject, 0.5f);
+
+                FindObjectOfType<ScoreManager>().AddScore(100);
+            }
+        }
+    }
+}
